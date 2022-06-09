918cc1265f139041502b83d3aa6e3b7ffacf72db
Kamil Dąbrowski <kamil@namekdev.net>
2022-06-09 02:26:09 +0200

    fix compilation error
    fix compilation error for Zig 0.9.1:
    ```
    system_sdk.zig:240:10: error: type '@typeInfo(@typeInfo(@TypeOf(std.child_process.ChildProcess.init)).Fn.return_type.?).ErrorUnion.error_set!*std.child_process.ChildProcess' does not support field access
        child.cwd = cwd;
             ^
    ```

--- glfw/system_sdk.zig
+++ glfw/system_sdk.zig
@@ -236,7 +236,7 @@ fn determineSdkRoot(allocator: std.mem.Allocator, org: []const u8, name: []const
 }
 
 fn exec(allocator: std.mem.Allocator, argv: []const []const u8, cwd: []const u8) !void {
-    var child = std.ChildProcess.init(argv, allocator);
+    var child = try std.ChildProcess.init(argv, allocator);
     child.cwd = cwd;
     _ = try child.spawnAndWait();
 }
